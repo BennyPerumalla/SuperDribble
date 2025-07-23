c3812c64f853a80ffd7235bf11e6ea65d1df623c
Mohammad Irshad Siddi <mohammadirshadsiddi@gmail.com>
2025-07-24 01:07:00 +0530

    Changed the UI into POPUP style & made the extension build ready

--- UI/vite.config.ts
+++ UI/vite.config.ts
@@ -4,14 +4,25 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',
   server: {
     host: "::",
     port: 8080,
+    open: "/"
   },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
     },
   },
 }));
